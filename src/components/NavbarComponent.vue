5ed6c4558fc7c96de35e9b22997cfa89a98167db
ikatza1967 <ikatzacarbon1@gmail.com>
2023-07-20 13:41:35 +0200

    añadido img al header

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,29 +1,65 @@
 <template>
-  <div class="container">
-  <nav>
-    <div class="logo">
-      <img src="../assets/img/glutenfree.jpg" alt="Logo de la empresa" />
-    </div>
-    <div class="fotoNavbar">
-      <img src="../assets/img/foto-NavBar.jpg" />
-    </div>
-    <div class="navbar-items">
-      <div class="navbar-item">
-        <input type="text" placeholder="Buscar producto" />
-      </div>
-      <div class="navbar-item">
-        <a href="#">Iniciar sesión</a>
-      </div>
-    </div>
+  <div class="navbar">
   
-  </nav>
-</div>
+      
+    <nav>
+      <div class="logo">
+        <div class="glutenfree" >
+          <img class="glutenf" src="../assets/img/glutenfree.jpg" alt="Logo de la empresa" />
+        </div> 
+         
+      </div>
+      <div class="navbar-items">
+        <div class="navbar-item">
+          <input class="buscar"  type="text" placeholder="Buscar producto" />
+        </div>
+      </div>
+      <div class="navbar-item">
+          <a href="#">Iniciar sesión</a>        
+      </div>
+
+    </nav>
+  </div>
 </template>
 <style scoped>
-.logo{
-height: auto;
-}
-.fotoNavbar{
 
+ .navbar{
+   background-image: url(../assets/img/imgnavbar.jpg);
+   width: 100%;
+   height:300px;
+    background-size: cover;
+   background-repeat: no-repeat;
+   background-position: center center;
+/* display: flex; */
+/* align-items: center; */
+
+} 
+ 
+ .glutenf{
+  width: 9rem;
+  
+}  
+
+.logo{
+  position:absolute;
+  top:14%;
+  left: 30px;
+} 
+.navbar-item a{
+  color: white;
+  text-decoration: none;
 }
+ .logo {
+  order: 1;
+  margin-right: 20px;
+  height: 550px;
+  width: 600px; 
+  
+}  
+.buscar{
+  height: 2em;
+  border-radius: 2rem;
+  
+}
+
 </style>
